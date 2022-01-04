1ddb851158a0cc705549fb958beb03149f28ce4e
btramel <bradtramel@gmail.com>
2022-01-04 13:11:01 -0600

    fixed issue where divs were stacking atop each other rather than starting from 0. added styles. no mobile support for now. adjusted sizing to be dynamic.

--- scripts.js
+++ scripts.js
@@ -1,42 +1,36 @@
 // sets important constants and variables
 const container = document.getElementById('container');
-const item = document.getElementsByClassName('grid-item');
+let items = document.getElementsByClassName('grid-item');
 let defaultSize = 16;
 
 function makeRows(rows, cols) {
-    // enters container div, sets the "--grid-rows" variable equal to "rows"
-    // same for --grid-cols and cols
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-
     // while c is less than the total area, create a div element
     // also adds text to the element
     // also adds classname "grid-item" to the element
-    for (c = 0; c < (rows * cols); c++) {
+    for ( c = 0; c < (rows * cols); c++ ) {
       let cell = document.createElement('div');
-    //   cell.innerText = (c + 1);
       cell.addEventListener('mouseover', function( event ) {
           event.target.style.backgroundColor = 'black';
-          event.target.style.color = 'black';
       });
       container.appendChild(cell).className = 'grid-item';
     };
-
-       //   reset etch-a-sketch
-      item.style.setProperty('backgroundColor', white);
-      item.style.setProperty('color', white);
-      
 };
 
 function reset() {
-    newGridSize = prompt('How large a grid would you like?');
-    makeRows(newGridSize, newGridSize);
+  // this removes the divs that were added initially, before starting over
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+}
+  let newGridSize = prompt('How large a grid would you like to make?', 16);
+  container.style.gridTemplateColumns = `repeat(${newGridSize}, 1fr)`;
+  makeRows(newGridSize, newGridSize);
 };
 
 document.getElementById('freshStart').onclick = function() {
-     reset()
+  reset();
 };
 
 
 // calls the function to defaultSize of 16 x 16
+container.style.gridTemplateColumns = `repeat(${defaultSize}, 1fr)`;
 makeRows(defaultSize, defaultSize);
